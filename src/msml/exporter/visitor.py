abf8f6aaf0759f0f03c2960983b8a60f1f302c49
Alexander Weigl <spamling@web.de>
2014-04-14 02:39:50 +0200

    New Version of Visitor Exporter Framework
    * now with dispatching
    * bug fix
    * api corrected

--- src/msml/exporter/visitor.py
+++ src/msml/exporter/visitor.py
@@ -26,6 +26,20 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 # endregion
 
+"""
+A exporter framework based upon the Visitor Pattern.
+
+This package consists of following important parts:
+
+ - Visitor
+ - VisitorDispatchable
+ - VisitorExporterFramework.
+
+*Visitor* defines the events that are called during traversing by VisitorExporterFramework.
+*VisitorDispatchable* provides additional mechanism to differ between
+types and tags of Meshes and ObjectElements.
+
+"""
 
 __authors__ = 'Alexander Weigl <uiduw@student.kit.edu>'
 __license__ = 'GPLv3'
@@ -33,18 +47,67 @@ __date__ = "2014-04-13"
 
 import abc
 from path import path
+from warnings import warn
+from collections import defaultdict
 
 from ..model import *
 from .base import Exporter
 
 
+class DispatcherWarning(MSMLWarning):
+    pass
+
+class DispatchError(MSMLError):
+    pass
+
 class Visitor(object):
+    """
+    Visitor should be inherited by the user to get events during traversing the msml scene graph.
+    The Visitor follows this rules:
+
+        * every list of elements are introduced with an *_begin
+        * every *_begin is closed by a matching *_end
+        * every list element as name *_element
+        * every begin event can deliver a product object that is delivered for every element event
+          and the matching event (sub contexts inclusive)
+
+    The processing order (bunny.msml.xml):
+
+      * msml_begin
+        * environment_begin
+          * environment_simulation
+          * environment_solver
+        * environment_end
+        * scene_begin
+          * object_begin
+            * object_mesh
+            * object_material_begin
+              * object_material_region_begin
+                * object_material_region_element
+                * object_material_region_element
+                * object_material_region_element
+              * object_material_region_end
+            * object_material_end
+            * object_constraints_begin
+              * object_constraint_begin
+                * object_constraint_element
+              * object_constraint_end
+            * object_constraints_end
+            * object_output_begin
+              * object_output_element
+            * object_output_end
+          * object_end
+        * scene_end
+      * msml_end
+      * write_export_file
+    """
+
+
     def __init__(self, exporter):
         self.exporter = exporter
 
     def msml_begin(self, msml_file):
         pass
-
 
     def environment_begin(self, _msml, env):
         pass
@@ -91,10 +154,10 @@ class Visitor(object):
     def object_material_region_element(self, _msml, _scene, _object, _material, _region, element):
         pass
 
-    def object_material_region_end(self, _msml, _scene, _object, _material, _region):
+    def object_material_region_end(self, _msml, _scene, _object, _material, _region, region):
         pass
 
-    def object_material_end(self, _msml, _scene, _object, _material):
+    def object_material_end(self, _msml, _scene, _object, _material, material):
         pass
 
     def object_constraints_begin(self, _msml, _scene, _object, constraints):
@@ -106,10 +169,10 @@ class Visitor(object):
     def object_constraint_element(self, _msml, _scene, _object, _constraints, _constraint, element):
         pass
 
-    def object_constraint_end(self, _msml, _scene, _object, _constraints, _constraint):
+    def object_constraint_end(self, _msml, _scene, _object, _constraints, _constraint, constraint):
         pass
 
-    def object_constraints_end(self, _msml, _scene, _object, _constraints):
+    def object_constraints_end(self, _msml, _scene, _object, _constraints, constraints):
         pass
 
     def object_output_begin(self, _msml, _scene, _object, outputs):
@@ -118,7 +181,7 @@ class Visitor(object):
     def object_output_element(self, _msml, _scene, _object, _output, output):
         pass
 
-    def object_output_end(self, _msml, _scene, _object, _output):
+    def object_output_end(self, _msml, _scene, _object, _output, output):
         pass
 
     def object_end(self, _msml, _scene, _object, sceneobject):
@@ -135,14 +198,140 @@ class Visitor(object):
         pass
 
 
+class MethodDispatcherMeta(type):
+    """
+    If you do not know about metaclasses see http://eli.thegreenplace.net/2011/08/14/python-metaclasses-by-example/
+    first.
+
+    This metaclass discovers methods in the class that are marked for
+    dispatching and gathered them with category and names.
+
+    """
+    def __new__(cls, name, bases, d):
+        #       print cls, name, bases, d
+
+        categories = defaultdict(dict)
+
+        for name, func in d.items():
+            if callable(func) and hasattr(func, '_dispatching'):
+                if func._dispatching:
+                    categories[func._dispatch_category][func._dispatch_name] = func
+
+                    #      print "Found:", categories
+
+        d["_dispatching"] = categories
+
+        def get_func(self, category, name, *args, **kwargs):
+            try:
+                f = self._dispatching[category][name]
+            except KeyError as e:
+                raise DispatchError(e)
+
+            return f(self, *args, **kwargs)
+
+        d['_dispatcher'] = get_func
+        return type.__new__(cls, name, bases, d)
+
+
+
+def disp_cat(category):
+    """
+    Provides a function dispatch marker for the given category.
+    """
+    def register_name(name):
+        """ Decorator for registering a function with a given name"""
+
+        def register_fn(func):
+            setattr(func, "_dispatching", True)
+            setattr(func, "_dispatch_category", category)
+            setattr(func, "_dispatch_name", name)
+            return func
+
+        return register_fn
+
+    return register_name
+
+
+##
+# Dispatch Marker
+disp_constraint = disp_cat("C")
+disp_mesh = disp_cat("M")
+disp_material = disp_cat("m")
+disp_output = disp_cat("O")
+#
+##
+
+
+class VisitorDispatchable(Visitor):
+    """
+    This class extends the Visitor by dispatching on mesh, constraint, output and material elements.
+    The dispatching happens on the tag or type attribute of ObjectElement or Mesh class.,
+
+
+    This creates a class that calls `object_constraint_element_fixedConstraint`
+    for every ObjectElement that is a constraint and has the tag `fixedConstraint`:
+
+    >>> class MyVisitor(VisitorDispatchable):
+    >>>     @disp_constraint('fixedConstraint')
+    >>>     def object_constraint_element_fixedConstraint(self, _msml, _scene, _object, _constraints, _constraint, element):
+    >>>         Printer.object_constraint_element_fixedConstraint(self, _msml, _scene, _object, _constraints, _constraint, element)
+
+    Use the dispatch markers: `disp_constraint, disp_mesh, disp_material, disp_output`
+
+    **DON'T** override the corresponding *_element functions (e.g. object_constraint_element). Every function should
+    have a different name.
+
+    Inheritance should not work.
+
+    """
+    __metaclass__ = MethodDispatcherMeta
+
+    def __init__(self, exporter):
+        Visitor.__init__(self, exporter)
+
+    def object_mesh(self, _msml, _scene, _object, mesh):
+        try:
+            return self._dispatcher('M', mesh.type, _msml, _scene, _object, mesh)
+        except DispatchError as e:
+            warn("return to default implementation for mesh type %s" % mesh.type, DispatcherWarning)
+            self.object_mesh_default(_msml, _scene, _object, mesh)
+
+    def object_mesh_default(self, _msml, _scene, _object, mesh):
+        pass
+
+    def object_output_element(self, _msml, _scene, _object, _output, output):
+        try:
+            return self._dispatcher('O', output.tag, _msml, _scene, _object, _output, _output)
+        except DispatchError as e:
+            warn("return to default implementation for output type %s" % output.tag, DispatcherWarning)
+            self.object_output_element_default(_msml, _scene, _object, _output, output)
+
+    def object_output_element_default(self, _msml, _scene, _object, _output, output):
+        pass
+
+    def object_constraint_element(self, _msml, _scene, _object, _constraints, _constraint, element):
+        try:
+            return self._dispatcher('C', element.tag, _msml, _scene, _object, _constraints, _constraint, element)
+        except DispatchError:
+            warn("return to default implementation for constraint type %s" % element.tag, DispatcherWarning)
+            self.object_constraint_element_default(_msml, _scene, _object, _constraints, _constraint, element)
+
+    def object_constraint_element_default(self, _msml, _scene, _object, _constraints, _constraint, element):
+        pass
+
+    def object_material_region_element(self, _msml, _scene, _object, _material, _region, element):
+        try:
+            return self._dispatcher('m', element.tag, _msml, _scene, _object, _material, _region, element)
+        except DispatchError:
+            warn("return to default implementation for material type %s" % element.tag, DispatcherWarning)
+            self.object_material_region_element_default(_msml, _scene, _object, _material, _region, element)
+
+    def object_material_region_element_default(self, _msml, _scene, _object, _material, _region, element):
+        pass
+
+
 class VisitorExporterFramework(Exporter):
     def __init__(self, msml_file, visitor_clazz):
-        """
-        Args:
-            executer (Executer)
-
-
-        """
         self.name = 'VisitorSekeleton'
         Exporter.__init__(self, msml_file)
         self.visitor_clazz = visitor_clazz
@@ -151,7 +340,9 @@ class VisitorExporterFramework(Exporter):
     def render(self):
         self.visitor = self.visitor_clazz(self)
         assert isinstance(self.visitor, Visitor)
+        self.visit()
 
+    def visit(self):
         _msml = self.visitor.msml_begin(self._msml_file)
 
         _env = self.visitor.environment_begin(_msml, self._msml_file.env)
@@ -160,7 +351,7 @@ class VisitorExporterFramework(Exporter):
         self.visitor.environment_end(_msml, _env, self._msml_file.env)
 
         # begin scene
-        _scene = self.visitor.scene_begin(self._msml_file.scene)
+        _scene = self.visitor.scene_begin(_msml, self._msml_file.scene)
 
         for obj in self._msml_file.scene:
             #begin object
@@ -170,39 +361,39 @@ class VisitorExporterFramework(Exporter):
             self.visitor.object_mesh(_msml, _scene, _so, obj.mesh)
 
             #begin material
-            _mat = self.visitor.object_material_begin(_msml, _scene, _so)
+            _mat = self.visitor.object_material_begin(_msml, _scene, _so, obj.material)
 
             for region in obj.material:
                 #begin region
-                _mr = self.visitor.object_material_region_begin(_so, _mat, region)
+                _mr = self.visitor.object_material_region_begin(_msml, _scene, _so, _mat, region)
                 for mat in region:
-                    self.visitor.object_material_region_element(_so, _mat, _mr, mat)
-                self.visitor.object_material_region_end(_so, _mat, _mr, region)
+                    self.visitor.object_material_region_element(_msml, _scene, _so, _mat, _mr, mat)
+                self.visitor.object_material_region_end(_msml, _scene, _so, _mat, _mr, region)
                 #end region
 
-            self.visitor.object_material_end(_so, _mat)
+            self.visitor.object_material_end(_msml, _scene, _so, _mat, obj.material)
             #end material
 
             #begin constraints
-            _css = self.visitor.object_constraints_begin(_msml, _scene, _so)
-            for cs in obj:
+            _css = self.visitor.object_constraints_begin(_msml, _scene, _so, obj.constraints)
+            for cs in obj.constraints:
                 #begin constraint set
                 _set = self.visitor.object_constraint_begin(_msml, _scene, _so, _css, cs)
 
-                for c in cs:
+                for c in cs.constraints:
                     self.visitor.object_constraint_element(_msml, _scene, _so, _css, _set, c)
 
                 self.visitor.object_constraint_end(_msml, _scene, _so, _css, _set, cs)
                 #end constraint set
 
-            self.visitor.object_constraints_end(_msml, _scene, _so, _css)
+            self.visitor.object_constraints_end(_msml, _scene, _so, _css, obj.constraints)
             #end constraints
 
             #begin output
-            _o = self.visitor.object_output_begin(_msml, _scene, _so)
+            _o = self.visitor.object_output_begin(_msml, _scene, _so, obj.output)
             for o in obj.output:
                 self.visitor.object_output_element(_msml, _scene, _so, _o, o)
-            self.visitor.object_output_end(_msml, _scene, _so, _o)
+            self.visitor.object_output_end(_msml, _scene, _so, _o, obj.output)
             #end output
 
             self.visitor.object_end(_msml, _scene, _so, obj)
@@ -214,7 +405,7 @@ class VisitorExporterFramework(Exporter):
         self.visitor.msml_end(_msml)
         #end file
 
-        self.export_file = self.visitor.write_export_file(path(self._msml_file), product)
+        self.export_file = self.visitor.write_export_file(path(self._msml_file), _msml)
 
     @abc.abstractmethod
     def execute(self):
---
+++ src/test_visitor_framework.py
@@ -0,0 +1,160 @@
+__author__ = 'Alexander Weig'
+
+import msml.exporter.visitor
+from msml.exporter.visitor import *
+
+import inspect
+def f(*args):
+    print inspect.stack()[1][3]
+
+class _Printer(object):
+    def __init__(self):
+        self.level = 0
+
+    def __getattr__(self, item):
+        lvl = self.level
+
+        if item.endswith("begin"):
+            self.level +=1
+        elif item.endswith("end"):
+            self.level -=1
+            lvl -= 1
+
+        def p(*args):
+            print "  " * lvl, " *", item
+        return p
+Printer = _Printer()
+
+
+class PrintVisitor(msml.exporter.visitor.Visitor):
+    def scene_end(self, _msml, _scene, scene):
+        Printer.scene_end(_msml, _scene, scene)
+
+    def object_constraints_begin(self, _msml, _scene, _object, constraints):
+        Printer.object_constraints_begin(_msml, _scene, _object, constraints)
+
+    def object_end(self, _msml, _scene, _object, sceneobject):
+        Printer.object_end(_msml, _scene, _object, sceneobject)
+
+    def object_constraint_element(self, _msml, _scene, _object, _constraints, _constraint, element):
+        Printer.object_constraint_element(_msml, _scene, _object, _constraints, _constraint, element)
+
+    def object_material_begin(self, _msml, _scene, _object, materials):
+        Printer.object_material_begin(_msml, _scene, _object, materials)
+
+    def environment_solver(self, _msml, _environment, solver):
+        Printer.environment_solver(_msml, _environment, solver)
+
+    def object_output_begin(self, _msml, _scene, _object, outputs):
+        Printer.object_output_begin(_msml, _scene, _object, outputs)
+
+    def msml_end(self, _msml):
+        Printer.msml_end(_msml)
+
+    def write_export_file(self, msml_file_path, product):
+        Printer.write_export_file(msml_file_path, product)
+
+    def object_sets_nodes(self, _msml, _scene, _object, _object_sets, node):
+        Printer.object_sets_nodes(_msml, _scene, _object, _object_sets, node)
+
+    def scene_begin(self, _msml, scene):
+        Printer.scene_begin(_msml, scene)
+
+    def environment_end(self, _msml, _environment, environment):
+        Printer.environment_end(_msml, _environment, environment)
+
+    def object_material_region_begin(self, _msml, _scene, _object, _material, region):
+        Printer.object_material_region_begin(_msml, _scene, _object, _material, region)
+
+    def object_constraint_begin(self, _msml, _scene, _object, _constraints, constraint):
+        Printer.object_constraint_begin(_msml, _scene, _object, _constraints, constraint)
+
+    def object_constraint_end(self, _msml, _scene, _object, _constraints, _constraint, constraint):
+        Printer.object_constraint_end(_msml, _scene, _object, _constraints, _constraint, constraint)
+
+    def object_constraints_end(self, _msml, _scene, _object, _constraints, constraints):
+        Printer.object_constraints_end(_msml, _scene, _object, _constraints, constraints)
+
+    def object_begin(self, _msml, _scene, object):
+        Printer.object_begin(_msml, _scene, object)
+
+    def object_material_region_end(self, _msml, _scene, _object, _material, _region, region):
+        Printer.object_material_region_end(_msml, _scene, _object, _material, _region, region)
+
+    def msml_begin(self, msml_file):
+        Printer.msml_begin(msml_file)
+
+    def object_material_end(self, _msml, _scene, _object, _material, material):
+        Printer.object_material_end(_msml, _scene, _object, _material)
+
+    def object_sets_end(self, _msml, _scene, _object):
+        Printer.object_sets_end(_msml, _scene, _object)
+
+    def object_sets_elements(self, _msml, _scene, _object, _object_sets, element):
+        Printer.object_sets_elements(_msml, _scene, _object, _object_sets, element)
+
+    def object_mesh(self, _msml, _scene, _object, mesh):
+        Printer.object_mesh(_msml, _scene, _object, mesh)
+
+    def object_output_element(self, _msml, _scene, _object, _output, output):
+        Printer.object_output_element(_msml, _scene, _object, _output, output)
+
+    def environment_begin(self, _msml, env):
+        Printer.environment_begin(_msml, env)
+
+    def object_sets_surfaces(self, _msml, _scene, _object, _object_sets, surface):
+        Printer.object_sets_surfaces(_msml, _scene, _object, _object_sets, surface)
+
+    def environment_simulation(self, _msml, _environment, simulation):
+        Printer.environment_simulation(_msml, _environment, simulation)
+
+    def object_material_region_element(self, _msml, _scene, _object, _material, _region, element):
+        Printer.object_material_region_element(_msml, _scene, _object, _material, _region, element)
+
+    def object_output_end(self, _msml, _scene, _object, _output,o):
+        Printer.object_output_end(_msml, _scene, _object, _output,o)
+
+    def object_sets_begin(self, _msml, _scene, _object, sets):
+        Printer.object_sets_begin(_msml, _scene, _object, sets)
+
+
+class PrintVisitorDisp(VisitorDispatchable, PrintVisitor):
+    @disp_constraint('fixedConstraint')
+    def object_constraint_element_fixedConstraint(self, _msml, _scene, _object, _constraints, _constraint, element):
+        Printer.object_constraint_element_fixedConstraint(self, _msml, _scene, _object, _constraints, _constraint, element)
+
+    @disp_mesh("linearTet")
+    def object_mesh_linearTet(self, _msml, _scene, _object, mesh):
+        Printer.object_mesh_linearTet(self, _msml, _scene, _object, mesh)
+
+    @disp_material("mass")
+    def object_material_region_element_mass(self, _msml, _scene, _object, _material, _region, element):
+        Printer.object_material_region_element_mass(self, _msml, _scene, _object, _material, _region, element)
+
+    @disp_material('linearElastic')
+    def object_material_region_element_linearElastic(self, _msml, _scene, _object, _material, _region, element):
+        Printer.object_material_region_element_linearElastic(self, _msml, _scene, _object, _material, _region, element)
+
+    @disp_material('indexgroup')
+    def object_material_region_element_indexgroup(self, _msml, _scene, _object, _material, _region, element):
+        Printer.object_material_region_element_indexgroup(self, _msml, _scene, _object, _material, _region, element)
+
+    @disp_output('displacement')
+    def object_output_element_displacement(self, _msml, _scene, _object, _output, output):
+        Printer.object_output_element_displacement(self, _msml, _scene, _object, _output, output)
+
+import msml.xml
+def main():
+    msml_file = msml.xml.load_msml_file("/home/weigl/workspace/msml/examples/BunnyExample/bunny.msml.xml")
+
+
+    exporter = msml.exporter.visitor.VisitorExporterFramework(msml_file, PrintVisitor)
+    exporter.render()
+
+
+    exporter = msml.exporter.visitor.VisitorExporterFramework(msml_file, PrintVisitorDisp)
+    exporter.render()
+
+
+if __name__ == "__main__":
+    main()
