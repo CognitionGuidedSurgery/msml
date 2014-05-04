97f80ef1ffa558bf9a1df656f74aacf0ed063bb4
Alexander Weigl <spamling@web.de>
2014-05-05 01:39:42 +0200

    fix ansitagcolor

--- src/msml/log.py
+++ src/msml/log.py
@@ -6,26 +6,29 @@ https://stackoverflow.com/questions/384076/how-can-i-color-python-logging-output
 """
 
 from __future__ import print_function
-import ansi
+try:
+    import ansi
 
-t = ansi.term()
-print = t.printr
-t.register("error",
-           ansi.style(foreground=ansi.Color16Table.White,
-                      background=ansi.Color16Table.Red))
+    t = ansi.term()
+    print = t.printr
+    t.register("error",
+               ansi.style(foreground=ansi.Color16Table.White,
+                          background=ansi.Color16Table.Red))
 
-t.register("ptype_error",
-           ansi.style(foreground=ansi.Color16Table.White,
-                      background=ansi.Color16Table.Red,
-                      option=ansi.Option.BOLD_ON))
+    t.register("ptype_error",
+               ansi.style(foreground=ansi.Color16Table.White,
+                          background=ansi.Color16Table.Red,
+                          option=ansi.Option.BOLD_ON))
 
-t.register("slot_sort_error",
-           ansi.style(foreground=ansi.Color16Table.White,
-                      background=ansi.Color16Table.Red,
-                      option=ansi.Option.BOLD_ON))
+    t.register("slot_sort_error",
+               ansi.style(foreground=ansi.Color16Table.White,
+                          background=ansi.Color16Table.Red,
+                          option=ansi.Option.BOLD_ON))
 
-t.register("ltype_error",
-           ansi.style(foreground=ansi.Color16Table.White,
-                      background=ansi.Color16Table.Red))
+    t.register("ltype_error",
+               ansi.style(foreground=ansi.Color16Table.White,
+                          background=ansi.Color16Table.Red))
 
-def p(s): t.cprint(s+"\n")
+    def p(s): t.cprint(s+"\n")
+except:
+    def p(s): print(s)
