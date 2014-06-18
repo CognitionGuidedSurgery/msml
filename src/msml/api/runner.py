e7e1684a7e7c2b88bf6068dd28928d071a4d8448
Stefan Suwelack <suwelack@kit.edu>
2014-06-18 14:09:21 +0200

    Fix for Drone.io

--- src/msml/api/runner.py
+++ src/msml/api/runner.py
@@ -4,16 +4,16 @@ import msml.frontend
 import msml.env
 from msml.model import *
 
-class Runner(object):
-    def __init__(self, file=None, exporter=None, output_dir = None):
-        self._file = file
-        self._exporter = exporter
-        self.output_dir = output_dir
-        self.
-
-        if(file):
-            #load msml file
-
-    def init_msml_system(self):
-        msml.env.load_user_file()
-        self._load_alphabet()
+# class Runner(object):
+#     def __init__(self, file=None, exporter=None, output_dir = None):
+#         self._file = file
+#         self._exporter = exporter
+#         self.output_dir = output_dir
+#         self.
+#
+#         if(file):
+#             #load msml file
+#
+#     def init_msml_system(self):
+#         msml.env.load_user_file()
+#         self._load_alphabet()
