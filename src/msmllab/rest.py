3d8c5bdad49589266ac3cb8c7087493c18b5a848
Alexander Weigl <spamling@web.de>
2014-08-03 10:24:37 +0200

    swagger for the win!

--- src/msmllab/rest.py
+++ src/msmllab/rest.py
@@ -5,8 +5,8 @@
 #
 # If you use this software in academic work, please cite the paper:
 # S. Suwelack, M. Stoll, S. Schalck, N.Schoch, R. Dillmann, R. Bendl, V. Heuveline and S. Speidel,
-#   The Medical Simulation Markup Language (MSML) - Simplifying the biomechanical modeling workflow,
-#   Medicine Meets Virtual Reality (MMVR) 2014
+# The Medical Simulation Markup Language (MSML) - Simplifying the biomechanical modeling workflow,
+# Medicine Meets Virtual Reality (MMVR) 2014
 #
 # Copyright (C) 2013-2014 see Authors.txt
 #
@@ -29,10 +29,11 @@
 __author__ = 'Alexander Weigl'
 __date__ = "2014-07-19"
 
-from jinja2 import Template
-import json
 from flask import Flask, request
-from flask.ext.restful import reqparse, abort, Api, Resource, fields, marshal
+from flask_restful import Api, Resource
+from flask_restful_swagger import swagger
+
+import msml.sorts
 from msml.frontend import App as MSMLApp
 
 
@@ -40,7 +41,8 @@ msmlapp = MSMLApp()
 alphabet = msmlapp.alphabet
 
 app = Flask(__name__)
-api = Api(app)
+rapi = Api(app)
+api = swagger.docs(rapi, apiVersion='0.1', basePath="i61p154.itec.uka.de:8522")
 
 
 def error(msg, no=500):
@@ -48,8 +50,8 @@ def error(msg, no=500):
 
 
 def slots(slots):
-    return [ (s.name, s.physical_type, s.logical_type)
-                for s in slots.values()]
+    return [(s.name, s.physical_type, s.logical_type)
+            for s in slots.values()]
 
 
 class OperatorResource(Resource):
@@ -60,13 +62,16 @@ class OperatorResource(Resource):
         except KeyError:
             return error("Operator not found")
 
+        print operator.sort.physical.__name__
+
         return {
-                'name': operator.name,
-                'input': slots(operator.input),
-                'output': slots(operator.output),
-                'parameter': slots(operator.parameters),
-                'meta': operator.meta
-            }
+            'name': operator.name,
+            'input': slots(operator.input),
+            'output': slots(operator.output),
+            'parameter': slots(operator.parameters),
+            'type': operator.sort.physical.__name__,
+            'meta': operator.meta
+        }
 
     def post(self, operator_name):
         try:
@@ -85,8 +90,55 @@ class OperatorResource(Resource):
         return result
 
 
+import msml.model
 
-import msml.sorts
+
+def create_resource(operator):
+    assert isinstance(operator, msml.model.Operator)
+
+
+    def post(_):
+        try:
+            arguments = get_arguments(operator.input, operator.parameters)
+            print arguments
+        except KeyError as e:
+            return error(e.message)
+        result = operator(**arguments)
+        return result
+
+    def get_parameters(operator):
+        def param(slot):
+            assert isinstance(slot, msml.model.Slot)
+            return {
+                "name": slot.name,
+                "description": slot.meta.get('doc', ""),
+                "required": True,
+                "allowMultiple": False,
+                'type': slot.sort.physical.__name__,
+                "paramType": "form"
+            }
+
+        return list(map(param, operator.input.values() + operator.parameters.values()))
+
+
+    meta = swagger.operation(
+        nickname=operator.name,
+        parameters=get_parameters(operator),
+        responseMessages=[
+            {
+                "code": 201,
+                "message": "Created. The URL of the created blueprint should be in the Location header"
+            },
+            {
+                "code": 405,
+                "message": "Invalid input"
+            }
+        ]
+    )
+
+    swaggt_post = meta(post)
+    resource =  type(operator.name, (Resource,), {'post': swaggt_post})
+    api.add_resource(resource, '/'+operator.name)
 
 
 def get_arguments(*dicts):
@@ -105,14 +157,14 @@ def get_arguments(*dicts):
     return arguments
 
 
-
-
 class ListOperators(Resource):
     def get(self):
         return [
             (name) for name in alphabet.operators.keys()
         ]
 
+for o in alphabet.operators.values():
+    create_resource(o)
 
 api.add_resource(ListOperators, '/operators/')
-api.add_resource(OperatorResource, '/operators/<string:operator_name>', endpoint = 'or')
+api.add_resource(OperatorResource, '/operators/<string:operator_name>', endpoint='or')
