4d2131da0e985f765f0c2c169789c6e1a48fa725
markus.stoll <m.stoll@dkfz.de>
2014-09-26 18:00:13 +0200

    Add: Optimization example

--- examples/PythonExamples/batchPressureOptimization.py
+++ examples/PythonExamples/batchPressureOptimization.py
@@ -1,3 +1,6 @@
+#requires SciPy: http://www.scipy.org/install.html
+
+
 import sys
 import copy
 from lxml import etree     
@@ -5,7 +8,8 @@ import os
 import ntpath
 from path import path
 
-
+import numpy as np
+from scipy import optimize
 
 from msml import frontend 
 import msml
@@ -19,11 +23,24 @@ from msml.exceptions import *
 
 msml_file = os.path.abspath('../CGALi2vLungs/lungs_new.xml')
 
-for p in range (5, 80, 5):
+p_v_history = dict()
+
+def f(p_array):
+    p = p_array[0]
+    if (p<-50 or p > 70): 
+        return 8119088*2 
     app = frontend.App(exporter='nsofa', output_dir='batchedPressure' + str(p))
     mfile = app._load_msml_file(msml_file)
     mfile.scene[0].constraints[0].constraints[1].pressure = str(p)
     mfile.scene[0].constraints[0].constraints[1].attributes['pressure'] =  str(p)
     mfile.scene[0].constraints[0].constraints[2].pressure =  str(p)
     mfile.scene[0].constraints[0].constraints[2].attributes['pressure'] =  str(p)
-    app.execute_msml(mfile)
+    mem = app.execute_msml(mfile)
+    aReturn = abs(mem._internal['volumeMeasure']['volume'] - 8400000)
+    p_v_history[p] = mem._internal['volumeMeasure']['volume']
+    return aReturn
+
+p0 = x0 = np.asarray((20))
+optimize.fmin_cg(f, x0)
+p_v_history
+    
