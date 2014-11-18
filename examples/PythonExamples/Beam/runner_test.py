25fbaea5c11430dfca89254f55995ee0a1f36fe8
Stefan Suwelack <suwelack@kit.edu>
2014-11-18 14:04:54 +0100

    Added python example for SimulationRunner

---
+++ examples/PythonExamples/Beam/runner_test.py
@@ -0,0 +1,20 @@
+__author__ = 'suwelack'
+import imp, sys
+
+import os
+import msml.api.simulation_runner as api
+
+
+
+
+#create one forward simulation
+
+
+
+msml_file = os.path.abspath('beamLinearDisp.msml.xml')
+
+myRunner = api.SimulationRunner(msml_file, "sofa", "/tmp/MSMLResults/")
+myRunner.update_variable('dispVar', [0,0,1.0])
+myRunner.run()
+referencePoints = myRunner.get_results('finalPointDisp', 'points')
+
