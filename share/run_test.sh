faa8d8e11d1265ed17a8c91c7c36d4944a1ec195
Alexander Weigl <spamling@web.de>
2014-12-02 19:37:41 +0100

    fix for testing

--- share/run_test.sh
+++ share/run_test.sh
@@ -1,4 +1,14 @@
-#!/bin/sh
+#!/bin/bash -x
+
+pwd 
+
+set
+
+#source /home/ubuntu/virtualenvs/venv-2.7.5/bin
+
+export PYTHONPATH=/home/ubuntu/msml/cbuild/bin
+
+cd /home/ubuntu/msml/src
 
 echo "Nose Test:"
 nosetests -vv --with-coverage --cover-tests --cover-inclusive --cover-package=msml --cover-erase -w src msmltest
