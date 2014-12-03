a9d4f1e940de8886d1940249abdbee8425fba013
Alexander Weigl <spamling@web.de>
2014-12-03 11:02:17 +0100

    sofa executable for circleci

--- share/install_msml.sh
+++ share/install_msml.sh
@@ -6,6 +6,7 @@ ROOT=/home/ubuntu/msml/
 cmake -DVTK_DIR=$ROOT/cache/VTK-6.1.0-BUILD/ \
       -DTETGEN_INCLUDE_DIRS=$ROOT/cache/tetgen1.5.0 \
       -DTETGEN_LIBRARY=$ROOT/cache/tetgen-build/libtet.a \
+      -DSOFA_EXECUTABLE=/home/ubuntu/msml/cache/sofa-build/bin/runSofa \
 	  $ROOT/operators
 make -j 3;
 exit 
