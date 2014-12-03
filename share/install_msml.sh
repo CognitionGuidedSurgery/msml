f64a45e46b79db25ebfb330ad7d9edc165b01856
Alexander Weigl <spamling@web.de>
2014-12-03 05:57:18 +0100

    dependency for ACVDOperators - ACVD, fix for testing

--- share/install_msml.sh
+++ share/install_msml.sh
@@ -3,8 +3,8 @@
 mkdir cbuild;
 cd cbuild;
 cmake -DVTK_DIR=../cache/VTK-6.1.0-BUILD/ \
-      -DTETGEN_INCLUDE_DIRS=../cache/tetgen \
+      -DTETGEN_INCLUDE_DIRS=../cache/tetgen1.5.0 \
       -DTETGEN_LIBRARY=../cache/tetgen-build/libtet.a \
       ../operators
 make -j 3;
-cd ..
+exit 
