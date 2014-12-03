4ba1caab06bb6a1dc236b7ffe6f997efc942c324
Alexander Weigl <spamling@web.de>
2014-12-03 05:39:40 +0100

    fix for testing

--- share/run_test.sh
+++ share/run_test.sh
@@ -11,7 +11,14 @@ export PYTHONPATH=/home/ubuntu/msml/cbuild/bin
 cd /home/ubuntu/msml/
 
 echo "Nose Test:"
-nosetests -vv --with-coverage --cover-tests --cover-inclusive --cover-package=msml --cover-erase -w src msmltest
+nosetests -vv --with-coverage  \
+	--cover-tests              \
+	--cover-inclusive          \
+	--cover-package=msml       \
+	--cover-erase              \
+	-w src                     \
+	msmltest
+
 error=$? #save errorlevel 
 
 echo "Submit Result:"
