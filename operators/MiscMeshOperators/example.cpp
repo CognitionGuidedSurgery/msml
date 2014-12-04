000288f48b1728c830496417bd90d768833b3018
Markus Stoll <m.stoll@dkfz.de>
2014-12-05 00:28:31 +0100

    Fix for CircleCI

--- operators/MiscMeshOperators/example.cpp
+++ operators/MiscMeshOperators/example.cpp
@@ -16,7 +16,7 @@ using namespace MSML;
 int main( int argc, char * argv[])
 {
   double cordsArray[] = { 0, 0, 9, 1, 2,3};
-  std::vector<double> coordinates(begin(cordsArray), end(cordsArray));
+  std::vector<double> coordinates(cordsArray, cordsArray + sizeof(cordsArray) / sizeof(double));
   Sources::GenerateSpheres(coordinates, 10.0, 10, 10, "C:/dev/msml/share/testdata/tmp/sphere_polydata.vtk");
 
 	return EXIT_SUCCESS;
