8e77e0ea98bd26d3088ecb5167e4dd1805d6c0ac
Stefan Suwelack <suwelack@kit.edu>
2014-12-04 16:01:07 +0100

    Changed ACVD operators to not use superbuild if already on the system

---
+++ operators/CMake/FindACVD.cmake
@@ -0,0 +1,33 @@
+set(ACVD_INCLUDE_PATHS "$ENV{ACVD_PATH}" "/opt/ACVD/include" "/usr/include")
+set(ACVD_LIB_PATHS "$ENV{ACVD_PATH}/lib" "/opt/ACVD/lib" "/usr/lib/")
+
+#set(ACVD_INCLUDE_PATHS "/usr/include")
+
+
+FIND_LIBRARY (ACVD_LIBRARY NAMES vtkDiscreteRemeshing vtkDiscreteRemeshingLibrary HINTS ${ACVD_LIB_PATHS}
+  DOC "The directory where the ACVD libraries reside"
+)    
+  
+FIND_PATH(ACVD_INCLUDE_DIR vtkDiscreteRemeshing/vtkAnisotropicDiscreteRemeshing.h ${ACVD_INCLUDE_PATHS})
+
+SET(ACVD_INCLUDE_DIRS
+${ACVD_INCLUDE_DIR}/vtkDiscreteRemeshing/
+${ACVD_INCLUDE_DIR}/Common/
+${ACVD_INCLUDE_DIR}/vtkVolumeProcessing/
+)
+
+IF("${ACVD_LIBRARY}" STREQUAL "ACVD_LIBRARY-NOTFOUND")
+SET(ACVD_FOUND "NO")
+ELSE()
+SET(ACVD_FOUND "YES")
+ENDIF ()
+
+SET(ACVD_LIBRARIES ${ACVD_LIBRARY})
+MARK_AS_ADVANCED(ACVD_LIBRARIES)
+
+
+
+  
+    
+
+    
--- src/msml/exporter/semantic_tools.py
+++ src/msml/exporter/semantic_tools.py
@@ -8,11 +8,54 @@ from .base import XMLExporter, Exporter
 from msml.exceptions import *
 import msml.env
 from rdflib import Graph, Literal, BNode, Namespace, RDF, URIRef
-from rdflib.namespace import DC, FOAF
+from rdflib.namespace import DC, FOAF, OWL, RDF, RDFS
+from collections import defaultdict
+from jinja2 import Template, Environment
+from os import path
+import importlib
+import inspect
+#from msml.model.baseClasses import msmlScene
 
-from msml.model import *
+from msml.model.base import *
 
 #from msml_namespace import MSMLRep
+
+#this class exports an ontology into python classes
+
+class OntoClassRef(URIRef):
+
+    def getClassName(self):
+        parts = self.split('#')
+        if len(parts) != 2:
+            print('Error, URIRef should contain 2 parts!')
+        return parts[1]
+
+    def getModuleName(self):
+        parts = self.split('#')
+        if len(parts) != 2:
+            print('Error, URIRef should contain 2 parts!')
+        pathParts = parts[0].split('/')
+
+        return pathParts[-1]
+
+    def toStr(self, superclasses=None, attributeDict=None):
+        returnStr =''
+        env = Environment(keep_trailing_newline=True, lstrip_blocks=False, trim_blocks=False)
+        superclass = ''
+        if not superclasses:
+            superclass = 'object'
+        else:
+            for theClass in superclasses:
+                superclass += theClass.getClassName() + ','
+            superclass = superclass[:-1]
+
+        template = env.from_string('\nclass {{name}}({{superclass}}):\n   pass\n ')
+        returnStr = template.render(name=self.getClassName(), superclass=superclass)
+
+
+        return
+
+
 
 
 class OntologyParser(object):
@@ -21,38 +64,104 @@ class OntologyParser(object):
         self._ontoGraph.parse(ontologyName)
         self._instanceGraph = Graph()
 
-    def parse_ontology_from_python_memory(self, representationNodeName, memory):
+    def createPythonModule(self, directory):
 
-        repNamespace = Namespace('http://www.msml.org/ontology/msmlRepresentation')
+        #for s in self._ontoGraph:
+        #    print(s)
 
-        #typeURI =
+        #get a list of all classes (are of type owl#Class)
+        classList = self._ontoGraph.subjects(RDF.type, OWL.Class)
 
-        #get type of data node
-        dataNodeType = value(representationNodeName,MSMLRep.isRepresentationOf, NONE)
+        #for all classes
+        #check if class has superclass, write signature using jinja template
+        modulesDict = defaultdict(list)
+        for currentClass in classList:
+            classURI = OntoClassRef(currentClass)
+            moduleName = classURI.getModuleName()
+            modulesDict[moduleName].append(classURI)
 
-        print(dataNodeType)
+        # className = URIRef('http://www.msml.org/ontology/msmlScene#Elements')
+        # classList2 = self._ontoGraph.objects(className, RDFS.subClassOf)
+        # classList3 = self._ontoGraph.objects(className, URIRef('http://www.w3.org/2000/01/rdf-schema#subClassOf'))
+        # #classList3 = self._ontoGraph.predicate_objects(className)
+        #
+        # if (className, None, None) in self._ontoGraph:
+        #    print "This graph contains triples about Bob!"
+        #
+        # for aClass in classList2:
+        #     print aClass
+        #
+        # for aClass in classList3:
+        #     print aClass
+        #
+        # print RDFS.subClassOf
 
-        #print(testTuples)
+        for moduleName in modulesDict:
+            establishedClasses = set()
+            currentModuleString = str()
 
-       # for s,p,o in self._ontoGraph:
-       #    print s,p,o
+            try:
+                longModuleName = '..' + moduleName
+                currentLoadedModule = importlib.import_module(longModuleName, 'msml.model.baseClasses.subpkg')
+                for name, obj in inspect.getmembers(currentLoadedModule):
+                    if inspect.isclass(obj):
+                        establishedClasses.add( obj.__name__)
+                #import statement in generated module
+                currentModuleString += 'from msml.model.baseClasses.'+moduleName+' import *\n\n'
+            except ImportError:
+                pass
 
-        print('finish')
+            theModule = modulesDict[moduleName]
 
-        #dataNodeName = self._ontoGraph.triples(representationNodeName,NONE, NONE)
-        #self._ontoGraph.
-        #create node in instance graph
+            for className in theModule:
+                #check if class is already there
+                currentModuleString  += self.addClassToString(className, establishedClasses)
+                print className.getClassName()
+
+            theFilename = path.abspath(path.join( directory, moduleName+'.py'))
+            print theFilename
+            file = open(theFilename, "w")
+            file.write(currentModuleString)
+            file.close()
+
+    def parseAttributes(self, className):
+        #get object properties within class domain
+        classList = self._ontoGraph.subjects(RDF.type, OWL.Class)
 
 
-        #get all properties of data node
+        #self._ontoGraph.bind("owl", OWL)
+
+        qres = self._ontoGraph.query(
+            """SELECT ?name  WHERE { ?name rdf:type owl:class }""")
+
+        for row in qres:
+            print(row)
 
 
-        #for all properties: get value from memory
+        print('complete')
 
-        #add properties to instance node
+        msml.sorts.conversion(typea, typeb,)
 
-        #for all children of representationNode: call recursiveParse
+    def addClassToString(self, className, establishedClasses):
 
+        if not className.getClassName() in establishedClasses:
+            finalString =' '
+            #check for superclass
+            superClasses = self._ontoGraph.objects(URIRef(className), RDFS.subClassOf)
+            classesList = list()
+            for superClass in superClasses:
+                if superClass:
+                    superClassURI = OntoClassRef(superClass)
+                    finalString += self.addClassToString( superClassURI, establishedClasses)
+                    classesList.append(superClassURI)
+
+            establishedClasses.add(className.getClassName())
+            #add in the attributes
+            attributes = self.parseAttributes(className)
+            finalString += className.toStr(superclasses=classesList, attributeDict =attributes )
+            return finalString
+        else:
+            return ''
 
 
     def __recursiveParse(self, currentNodeName, memory):
