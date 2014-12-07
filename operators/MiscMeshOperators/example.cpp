38afa914be00f0b3acfd95d7fab91bdade08eb3d
Markus Stoll <m.stoll@dkfz.de>
2014-12-07 15:06:47 +0100

    python example: Color mesh  fxed

--- operators/MiscMeshOperators/example.cpp
+++ operators/MiscMeshOperators/example.cpp
@@ -11,14 +11,21 @@
 #include <IOHelper.h>
 #include <Sources.h>
 
+#include <stdio.h>
+#include <direct.h>
+
+
 using namespace MSML;
 
 int main( int argc, char * argv[])
 {
-  double cordsArray[] = { 0, 0, 9, 1, 2,3};
-  std::vector<double> coordinates(cordsArray, cordsArray + sizeof(cordsArray) / sizeof(double));
-  Sources::GenerateSpheres(coordinates, 10.0, 10, 10, "C:/dev/msml/share/testdata/tmp/sphere_polydata.vtk");
 
+  //double cordsArray[] = { 0, 0, 9, 1, 2,3};
+  //std::vector<double> coordinates(cordsArray, cordsArray + sizeof(cordsArray) / sizeof(double));
+  //Sources::GenerateSpheres(coordinates, 10.0, 10, 10, "C:/dev/msml/share/testdata/tmp/sphere_polydata.vtk");
+  //_chdir("C:\\dev\\msml\\examples\\ImageEample\\tmp");
+  PostProcessingOperators::ImageSum("aSphere*.vti", false, "sum.vti");
+   
 	return EXIT_SUCCESS;
 }
 
