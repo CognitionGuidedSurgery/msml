70fbfbee0c9b688dded789166d1d7066cc5dda40
Markus Stoll <m.stoll@dkfz.de>
2014-12-07 23:31:29 +0100

    more fixes for  Test examples #183

--- examples/PythonExamples/batchPressureOptimization.py
+++ examples/PythonExamples/batchPressureOptimization.py
@@ -45,10 +45,9 @@ def f_to_minimize(p_array):
     l = Lungs(msml_file, p)
     volume = l() 
     return abs(volume- 8400000)
-lost
+
+
 msml_file = os.path.abspath('../CGALi2vLungs/Lungs_new.xml')
-
-
 p0 = x0 = np.asarray((20))
 pn = optimize.fmin_cg(f_to_minimize, x0, epsilon=0.1)
 print pn
