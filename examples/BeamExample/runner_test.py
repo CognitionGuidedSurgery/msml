4edc56a53a505f04d63a318ca3992ceeb90611d0
song <gelato1205@hotmail.com>
2014-12-22 18:33:35 +0100

    updated runner_test

--- examples/BeamExample/runner_test.py
+++ examples/BeamExample/runner_test.py
@@ -7,11 +7,11 @@ import msml.api.simulation_runner as api
 
 #create one forward simulation
 
-msml_file = os.path.abspath('beamLinearForceDisp.msml.xml')
+msml_file = os.path.abspath('beamLinearDisp.msml.xml')
 #msml_file = os.path.abspath('../CGALi2vLungs/Lungs_new.xml')
 
 myRunner = api.SimulationRunner(msml_file, "sofa", "/tmp/MSMLResults/") #the output directory is an absolute path !
-#myRunner.update_variable('dispVar', [0,0,1.0])
+myRunner.update_variable('dispVar', [0,0,1.0])
 myRunner.run()
 referencePoints = myRunner.get_results('finalPointDisp', 'points')
 
