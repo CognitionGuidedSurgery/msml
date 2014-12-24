c587b43bf7d620213fb407fc3a80a7fee42b4550
Alexander Weigl <areku@users.noreply.github.com>
2014-12-24 23:20:15 +0100

    change VTK_DIR to install dir

--- share/install_msml.sh
+++ share/install_msml.sh
@@ -3,10 +3,10 @@
 mkdir cbuild;
 cd cbuild;
 ROOT=/home/ubuntu/msml/
-cmake -DVTK_DIR=$ROOT/cache/VTK-6.1.0-BUILD/ \
+cmake -DVTK_DIR=$ROOT/cache/VTK-6.1.0-INSTALL/lib/cmake/vtk-6.1/ \
       -DTETGEN_INCLUDE_DIRS=$ROOT/cache/tetgen1.5.0 \
       -DTETGEN_LIBRARY=$ROOT/cache/tetgen-build/libtet.a \
       -DSOFA_EXECUTABLE=/home/ubuntu/msml/cache/sofa-build/bin/runSofa \
-	  $ROOT/operators
+       $ROOT/operators
 make -j 3;
 exit 
