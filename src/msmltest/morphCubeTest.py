0d6ec53abc5e177df021a0eb59de262813d64543
Daniel Schaubach <dschauba@googlemail.com>
2015-01-08 15:40:25 +0100

    Added nose test/example for computeDiceCoefficient (in morphCubeTest/MorphCube-Example).

--- src/msmltest/morphCubeTest.py
+++ src/msmltest/morphCubeTest.py
@@ -17,7 +17,12 @@ class MorphCubeTest(TestCase):
                        executor='sequential', add_search_path=[test_common.MSML_ALPHABET_DIR])
         self.mf = self.app._load_msml_file(msml_file)
         mem = self.app.execute_msml(self.mf, ) 
+        #compare volume of mesh before and after morphing, volume after morphing should be less than volume before morphing
         originalVolume = mem._internal['OriginalPistonSurfaceVolume']['volume']
         morphedVolume = mem._internal['MorphedPistonSurfaceVolume']['volume']
         self.assertTrue(morphedVolume<originalVolume)
+        
+        #now, check the dice coefficient (should be above 0.5, below 1) 
+        diceCoefficient = mem._internal['computeDice']['diceCoefficient']
+        self.assertTrue(diceCoefficient>0.5 and diceCoefficient<1)
         
