818d8b82b3ecf0b36bdc40e10a4c8a38fb611b0d
song <gelato1205@hotmail.com>
2015-01-09 17:40:09 +0100

    Fix to sofa exporter to support multiple displacement values in displacement constraint

--- examples/BeamExample/runner_test.py
+++ examples/BeamExample/runner_test.py
@@ -11,7 +11,7 @@ msml_file = os.path.abspath('beamLinearDisp.msml.xml')
 #msml_file = os.path.abspath('../CGALi2vLungs/Lungs_new.xml')
 
 myRunner = api.SimulationRunner(msml_file, "sofa", "/tmp/MSMLResults/") #the output directory is an absolute path !
-myRunner.update_variable('dispVar', [0,0,1.0])
+myRunner.update_variable('dispVar', [0,0,2.0])
 myRunner.run()
 referencePoints = myRunner.get_results('finalPointDisp', 'points')
 
