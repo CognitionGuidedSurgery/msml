168f68227e4f1426137a50e879833fcfbad9e747
Nicolai Schoch <nicolai.schoch@iwr.uni-heidelberg.de>
2015-04-12 16:24:11 +0200

    add complete set of Mitral Valve Simulation Pre- & Post-Processing Operators for MSML-Pipeline

---
+++ src/msml/ext/mvGeometryAnalytics.py
@@ -0,0 +1,84 @@
+# ======================================================================
+# Given the 3D volume mesh of a mitral valve, this script computes
+# - the bounds of bounding box,
+# - the center of the bounding box,
+# - the point in the middle of the top plane of the bounding box  (!!!)
+# - the average 'radius' of the top plane of the bounding box     (!!!)
+# for an automated setup of the HiFlow3-based MVR-Simulation.
+# 
+# NOTE: 3dvolumeMesh.vtu is called uGrid here.
+# 
+# How to run the script:
+#   python mvGeometryAnalyzer.py 3dvolumeMesh.vtu analyticsOutputFilename.txt
+# 
+# Author: Nicolai Schoch, EMCL; 2015-04-12.
+# ======================================================================
+
+__author__ = 'schoch'
+import numpy as np
+import sys
+import vtk
+#from .msmlvtk import * # NEEDED?!
+
+
+def mvGeometry_Analyzer(inputfilename, outputfilename):
+	# ======================================================================
+	# get system arguments -------------------------------------------------
+	ugFilename = inputfilename
+	outputFilename_ = outputfilename
+	
+	print " "
+	print "=================================================================================================="
+	print "=== Execute Python script to analyze MV geometry in order for the HiFlow3-based MVR-Simulation ==="
+	print "=================================================================================================="
+	print " "
+	
+	# ======================================================================
+	# read in files: -------------------------------------------------------
+	# read in 3d valve mesh
+	vtureader = vtk.vtkXMLUnstructuredGridReader()
+	vtureader.SetFileName(ugFilename)
+	vtureader.Update()
+	uGrid = vtureader.GetOutput()
+	
+	# ======================================================================
+	# compute bounding box, etc. -------------------------------------------
+	# methods can be found in vtkDataSet
+	
+	# Get the center of the bounding box.
+	center = np.zeros(3)
+	uGrid.GetCenter(center)
+	# Return a pointer to the geometry bounding box
+	# (notation: [xmin,xmax, ymin,ymax, zmin,zmax].)
+	bounds = np.zeros(6)
+	uGrid.GetBounds(bounds)
+	# compute point in the middle of top plane
+	midPtTop = np.zeros(3)
+	midPtTop[0] = center[0]
+	midPtTop[1] = center[1]
+	midPtTop[2] = bounds[5]
+	# compute the average 'radius' of the top plane of the bounding box
+	# compute x/y-distance-from-mid-to-border
+	xDist = midPtTop[0] - bounds[0]
+	yDist = midPtTop[1] - bounds[2]
+	# compute average of x/y-distance-from-mid-to-border (corresponds to annulus radius)
+	avDist = (xDist + yDist) / 2.0
+	annulusRadius = avDist
+	
+	print "MV geometry analysis: DONE."
+	
+	# ======================================================================
+	# write results to txt file --------------------------------------------
+	f = open(outputFilename_, 'w')
+	
+	# produce outputAnalytics string and write into file
+	analyticsString = 'midPtTop:(' + str(midPtTop[0]) + ',' + str(midPtTop[1]) + ',' + str(midPtTop[2]) + ');\n' + 'annulusRadius:' + str(annulusRadius) + ';\n'
+	f.write(analyticsString)
+	
+	# close stream
+	f.close()
+	
+	# ======================================================================
+	print "Writing MVgeometryAnalyticsData output file: DONE."
+	print "=================================================="
+	print " "
---
+++ src/msml/ext/vonMisesStressComputation_mvrPostProcessingAnalytics.py
@@ -0,0 +1,154 @@
+# ======================================================================
+# Given vtu/pvtu file(s) resulting from an MVR elasticity simulation 
+# (the solution files hence contain three scalar valued arrays named 
+# 'u0', 'u1' and 'u2' as PointData), this vtk-based python script
+# warps the initial/original mesh geometry by means of the displacement
+# vector, and computes the Cauchy strain tensor and the von Mises stress.
+# 
+# The script's output is the following:
+#  - vtu file that contains all the data of the
+#    original file with modified coordinates of the points and the 
+#    displacement vector as additional PointData and the strain tensor 
+#    along with the von Mises stress as additional CellData.
+# 
+# How to run the script:
+#   python calculator.py ./input/myInput.(p)vtu ./output/myOutput.vtu
+# 
+# Author: Nicolai Schoch, EMCL; 2015-04-12.
+# ======================================================================
+
+__author__ = 'schoch'
+import sys
+import vtk
+#from .msmlvtk import * # NEEDED?!
+
+
+def compute_vonMisesStress_for_MV(inputfilename, outputfilename):
+	# ======================================================================
+	# get system arguments -------------------------------------------------
+	# Path to input file and name of the output file
+	#inputfilename = sys.argv[1]
+	#outputfilename = sys.argv[2]
+	
+	print " "
+	print "=================================================================================================="
+	print "=== Execute Python script to analyze MV geometry in order for the HiFlow3-based MVR-Simulation ==="
+	print "=================================================================================================="
+	print " "
+	
+	# ======================================================================
+	# Read file
+	if inputfilename[-4] == 'p':
+		reader = vtk.vtkXMLPUnstructuredGridReader()
+		reader.SetFileName(inputfilename)
+		reader.Update()
+	else:
+		reader = vtk.vtkXMLUnstructuredGridReader()
+		reader.SetFileName(inputfilename)
+		reader.Update()
+	
+	print "Reading input files: DONE."
+	
+	# ======================================================================
+	# Compute displacement vector
+	calc = vtk.vtkArrayCalculator()
+	calc.SetInput(reader.GetOutput())
+	calc.SetAttributeModeToUsePointData()
+	calc.AddScalarVariable('x', 'u0', 0)
+	calc.AddScalarVariable('y', 'u1', 0)
+	calc.AddScalarVariable('z', 'u2', 0)
+	calc.SetFunction('x*iHat+y*jHat+z*kHat')
+	calc.SetResultArrayName('DisplacementSolutionVector')
+	calc.Update()
+	
+	# ======================================================================
+	# Compute strain tensor
+	derivative = vtk.vtkCellDerivatives()
+	derivative.SetInput(calc.GetOutput())
+	derivative.SetTensorModeToComputeStrain()
+	derivative.Update()
+	
+	# ======================================================================
+	# Compute von Mises stress
+	calc = vtk.vtkArrayCalculator()
+	calc.SetInput(derivative.GetOutput())
+	calc.SetAttributeModeToUseCellData()
+	calc.AddScalarVariable('Strain_0', 'Strain', 0)
+	calc.AddScalarVariable('Strain_1', 'Strain', 1)
+	calc.AddScalarVariable('Strain_2', 'Strain', 2)
+	calc.AddScalarVariable('Strain_3', 'Strain', 3)
+	calc.AddScalarVariable('Strain_4', 'Strain', 4)
+	calc.AddScalarVariable('Strain_5', 'Strain', 5)
+	calc.AddScalarVariable('Strain_6', 'Strain', 6)
+	calc.AddScalarVariable('Strain_7', 'Strain', 7)
+	calc.AddScalarVariable('Strain_8', 'Strain', 8)
+	calc.SetFunction('sqrt( (2*1400*Strain_0 + 56933*(Strain_0+Strain_4+Strain_8))^2 + (2*1400*Strain_4 + 56933*(Strain_0+Strain_4+Strain_8))^2 + (2*1400*Strain_8 + 56933*(Strain_0+Strain_4+Strain_8))^2 - ( (2*1400*Strain_0 + 56933*(Strain_0+Strain_4+Strain_8))*(2*1400*Strain_4 + 56933*(Strain_0+Strain_4+Strain_8)) ) - ( (2*1400*Strain_0 + 56933*(Strain_0+Strain_4+Strain_8))*(2*1400*Strain_8 + 56933*(Strain_0+Strain_4+Strain_8)) ) - ( (2*1400*Strain_4 + 56933*(Strain_0+Strain_4+Strain_8))*(2*1400*Strain_8 + 56933*(Strain_0+Strain_4+Strain_8)) ) + 3 * ((2*1400*Strain_3)^2 + (2*1400*Strain_6)^2 + (2*1400*Strain_7)^2) )')
+	calc.SetResultArrayName('vonMisesStress_forMV_mu1400_lambda56933')
+	calc.Update()
+	
+	print "Computation of displacement vectors, Cauchy strain and vom Mises stress: DONE."
+	
+	# ======================================================================
+	# Define dummy variable; get output of calc filter
+	dummy = calc.GetOutput()
+	
+	# Get point data arrays u0, u1 and u2
+	pointData_u0 = dummy.GetPointData().GetArray('u0')
+	pointData_u1 = dummy.GetPointData().GetArray('u1')
+	pointData_u2 = dummy.GetPointData().GetArray('u2')
+	
+	# Set scalars
+	dummy.GetPointData().SetScalars(pointData_u0)
+	
+	# ======================================================================
+	# Warp by scalar u0
+	warpScalar = vtk.vtkWarpScalar()
+	warpScalar.SetInput(dummy)
+	warpScalar.SetNormal(1.0,0.0,0.0)
+	warpScalar.SetScaleFactor(1.0)
+	warpScalar.SetUseNormal(1)
+	warpScalar.Update()
+	
+	# Get output and set scalars
+	dummy = warpScalar.GetOutput()
+	dummy.GetPointData().SetScalars(pointData_u1)
+	
+	# ======================================================================
+	# Warp by scalar u1
+	warpScalar = vtk.vtkWarpScalar()
+	warpScalar.SetInput(dummy)
+	warpScalar.SetNormal(0.0,1.0,0.0)
+	warpScalar.SetScaleFactor(1.0)
+	warpScalar.SetUseNormal(1)
+	warpScalar.Update()
+	
+	# Get output and set scalars
+	dummy = warpScalar.GetOutput()
+	dummy.GetPointData().SetScalars(pointData_u2)
+	
+	# ======================================================================
+	# Warp by scalar u2
+	warpScalar = vtk.vtkWarpScalar()
+	warpScalar.SetInput(dummy)
+	warpScalar.SetNormal(0.0,0.0,1.0)
+	warpScalar.SetScaleFactor(1.0)
+	warpScalar.SetUseNormal(1)
+	warpScalar.Update()
+	
+	# Get ouput and add point data arrays that got deleted earlier
+	dummy = warpScalar.GetOutput()
+	dummy.GetPointData().AddArray(pointData_u0)
+	dummy.GetPointData().AddArray(pointData_u1)
+	
+	# ======================================================================
+	# Write output to vtu
+	writer = vtk.vtkXMLUnstructuredGridWriter()
+	writer.SetDataModeToAscii()
+	writer.SetFileName(outputfilename)
+	writer.SetInput(dummy)
+	writer.Write()
+	
+	# ======================================================================
+	print "Writing Extended VTU incl. von Mises Stress information: DONE."
+	print "=============================================================="
+	print " "
