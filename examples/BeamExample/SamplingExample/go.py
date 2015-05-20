62ec5c7e457da2c0443d5b29f535a666a779824f
Markus Stoll <m.stoll@dkfz.de>
2015-05-20 10:56:29 +0200

    Add for Sampling example: MSML script to load multiple boundary conditions form csv and run multiple simulations. Todos: Statistics for input and post processing (aggregation)

---
+++ examples/BeamExample/SamplingExample/go.py
@@ -0,0 +1,81 @@
+__author__ = 'Markus Stoll, Chen Song'
+import sys
+sys.path.append('../../../src/');
+
+
+ 
+import copy
+from lxml import etree    
+import os
+import ntpath
+import shutil
+from path import path
+ 
+ 
+ 
+from msml import frontend
+import msml
+import msml.env
+import msml.model
+import msml.run
+import msml.xml
+import msml.exporter
+import msml.exceptions
+from msml.exceptions import *
+from msml.frontend import App
+from csv import reader
+import multiprocessing
+ 
+ 
+def createAndRunHnnSimulation(args_list): #create a simulation instance with given parameter and run it.
+    aHeadneck_simulation = headneck(args_list['msml_file'], args_list['i'], args_list['scenarioDisp'])
+    return aHeadneck_simulation()
+   
+class headneck(object): 
+    def __init__(self, msml_filename, id, scenarioDisp):
+        self.id = id
+        self.app = App(seq_parallel=True, exporter='nsofa', output_dir= NAME + str(id), executor='sequential')
+        self.mf = self.app._load_msml_file(msml_filename)
+        self._scenarioDisp = scenarioDisp
+   
+    def __call__(self):
+        self.app.memory_init_file = {
+            "dispVar":self._scenarioDisp, 
+        }
+        mem = self.app.execute_msml(self.mf, )
+        return #mem._internal['volumeMeasure']['volume']
+ 
+ 
+NAME = "OUT___"
+if __name__ == '__main__': #for parallel processing compatibility
+   
+    
+    msml_file_name ='beamLinearDisp.msml.xml'
+        
+    args_list = []
+    startDir = os.getcwd()
+    headneck_simulations = []
+    
+    csv_file_name = "mc_values.csv"
+    scenarios = reader(open(csv_file_name, "rb"), delimiter=',', dialect='excel')
+
+    #collect parameters for all simulation runs
+    i=1
+    for scenarioRow in scenarios: #one  scenario = one line in csv file = one simulation run = one simulation output folder
+        if (i>0):
+            args = { 'i':i, 'msml_file': msml_file_name, 'scenarioDisp': scenarioRow } 
+            args_list.append(dict(args)) #copy            
+        i=i+1
+
+    #run simulations
+    for j in range(0,i-1):  
+        os.chdir( startDir )    
+        createAndRunHnnSimulation(args_list[j]) 
+   
+    #num_of_workers =   multiprocessing.cpu_count() -1
+    #pool = multiprocessing.Pool(5)
+    #pool.map(createAndRunHnnSimulation, args_list)
+   
+    
+ 
+   
