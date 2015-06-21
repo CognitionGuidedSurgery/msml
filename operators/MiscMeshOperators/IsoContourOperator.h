7991b90abbf7d161bf899572b71e45d2107b4ddb
Chen Song <chen.song@iwr.uni-heidelberg.de>
2015-06-21 14:34:42 +0200

    IsoCountourOperator

---
+++ operators/MiscMeshOperators/IsoContourOperator.h
@@ -0,0 +1,26 @@
+#include "../MSML_Operators.h"
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <stdlib.h> 
+#include <vtkXMLUnstructuredGridReader.h>
+#include <vtkXMLUnstructuredGridWriter.h>
+#include <vtkUnstructuredGrid.h>
+#include <vtkSmartPointer.h>
+#include <vtkPoints.h>
+#include <vtkDataSetSurfaceFilter.h>
+#include <vtkPolyData.h>
+#include <vtkXMLPolyDataWriter.h>
+#include <cmath>
+#include <vtkPolyDataNormals.h>
+#include <vtkFloatArray.h>
+#include <vtkPointData.h>
+
+namespace MSML
+{
+	namespace IsoContourOperator
+	{
+		LIBRARY_API void IsoContourOperator(const char* data_directory, const char* initial_position, const char* final_position, const char* weight_table);
+	}
+}
