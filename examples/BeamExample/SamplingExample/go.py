4929c7b292925d82ddc4aac0130ee45bdabed295
markus.stoll <m.stoll@dkfz.de>
2015-08-14 09:05:08 +0200

    Sampling example: Added imagesum to compare with IsoContour

--- examples/BeamExample/SamplingExample/go.py
+++ examples/BeamExample/SamplingExample/go.py
@@ -29,21 +29,23 @@ import multiprocessing
  
  
 def createAndRunHnnSimulation(args_list): #create a simulation instance with given parameter and run it.
-    aHeadneck_simulation = headneck(args_list['msml_file'], args_list['i'], args_list['scenarioDisp'], args_list['scenarioResultMesh'])
+    aHeadneck_simulation = headneck(args_list['msml_file'], args_list['i'], args_list['scenarioDisp'], args_list['scenarioResultMesh'], args_list['resultImage'])
     return aHeadneck_simulation()
    
 class headneck(object): 
-    def __init__(self, msml_filename, id, scenarioDisp, scenarioResultMesh):
+    def __init__(self, msml_filename, id, scenarioDisp, scenarioResultMesh, resultImage):
         self.id = id
         self.app = App(seq_parallel=True, exporter='nsofa', output_dir= NAME + str(id), executor='sequential')
         self.mf = self.app._load_msml_file(msml_filename)
         self._scenarioDisp = scenarioDisp
         self._scenarioResultMesh = scenarioResultMesh
+        self.resultImage = resultImage
    
     def __call__(self):
         self.app.memory_init_file = {
             "dispVar":self._scenarioDisp,
-			"resultMesh":self._scenarioResultMesh
+			"resultMesh":self._scenarioResultMesh,
+            "resultImage":self.resultImage            
         }
         mem = self.app.execute_msml(self.mf, )
         return self._scenarioResultMesh
@@ -62,17 +64,19 @@ if __name__ == '__main__': #for parallel processing compatibility
     csv_file_name = "stdnorm_mc_10k.csv"
     scenarios = reader(open(csv_file_name, "rb"), delimiter=',', dialect='excel')
     result_vtus = list()
+    result_vtis = list()
     
     #collect parameters for all simulation runs
     i=1
     for scenarioRow in scenarios: #one  scenario = one line in csv file = one simulation run = one simulation output folder
         if (i>0):
             vec = [0,0, float(scenarioRow[0])*0.01]
-            args = { 'i':i, 'msml_file': msml_file_name, 'scenarioDisp': vec, 'scenarioResultMesh' : '../' + NAME + str(i) + '.vtu' } 
+            args = { 'i':i, 'msml_file': msml_file_name, 'scenarioDisp': vec, 'scenarioResultMesh' : '../' + NAME + str(i) + '.vtu', 'resultImage': '../' + NAME + str(i) + '.vti'} 
             result_vtus.append( NAME + str(i) + '.vtu' )
+            result_vtis.append( NAME + str(i) + '.vti' )
             args_list.append(dict(args)) #copy            
         i=i+1
-        if i>2:
+        if i>10:
             break
 
     #run simulations
@@ -83,8 +87,11 @@ if __name__ == '__main__': #for parallel processing compatibility
     
     os.chdir( startDir )
     vtus = ['vtus', 'vtus2']
-    weights =  [0.5, 0.5]
+    weights =  [0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1]
     MiscOps.IsoContourOperator('', 'init.vtu', result_vtus, weights)
+    
+    MiscOps.ImageSum(NAME+'*' + '.vti', True, 'imgSum.vti');
+    
         
 		
    
