f428d3cc7e0b35c3d39677a25724809651f65bcc
songcn <chen.song@h-its.org>
2015-10-13 15:56:15 +0200

    some clean-up

--- examples/BeamExample/SamplingExample/go.py
+++ examples/BeamExample/SamplingExample/go.py
@@ -51,18 +51,23 @@ class headneck(object):
         return self._scenarioResultMesh
  
  
-NAME = "OUT_MC_DIRNEW_"
+NAME = "OUT_MC100_DIRNEW_"
+COUNTER = 100
+DIR = './MC100/'
+os.mkdir(DIR)
+
 if __name__ == '__main__': #for parallel processing compatibility
    
-    
+    shutil.copy('./init.vtu', DIR + 'init.vtu')   
+ 
     msml_file_name ='beamLinearDisp.msml.xml'
         
     args_list = []
     startDir = os.getcwd()
     headneck_simulations = []
     
-    #csv_file_name = "stdnorm_mc_10k.csv"
-    csv_file_name = "test_mc_100.csv"
+    csv_file_name = "./samples/test_mc_100.csv"
+    #csv_file_name = "./samples/reference.csv"
 
     scenarios = reader(open(csv_file_name, "rb"), delimiter=',', dialect='excel')
     result_vtus = list()
@@ -72,13 +77,13 @@ if __name__ == '__main__': #for parallel processing compatibility
     i=1
     for scenarioRow in scenarios: #one  scenario = one line in csv file = one simulation run = one simulation output folder
         if (i>0):
-            vec = [0,0, float(scenarioRow[0])*0.0001]
-            args = { 'i':i, 'msml_file': msml_file_name, 'scenarioDisp': vec, 'scenarioResultMesh' : '../' + NAME + str(i) + '.vtu', 'resultImage': '../' + NAME + str(i) + '.vti'} 
+            vec = [0,0, float(scenarioRow[0])*0.01]
+            args = { 'i':i, 'msml_file': msml_file_name, 'scenarioDisp': vec, 'scenarioResultMesh' : '../' + DIR + NAME + str(i) + '.vtu', 'resultImage': '../' + DIR + NAME + str(i) + '.vti'} 
             result_vtus.append( NAME + str(i) + '.vtu' )
             result_vtis.append( NAME + str(i) + '.vti' )
             args_list.append(dict(args)) #copy            
         i=i+1
-        if i>100:
+        if i>COUNTER:
             break
 
     #run simulations
@@ -88,20 +93,27 @@ if __name__ == '__main__': #for parallel processing compatibility
         createAndRunHnnSimulation(args_list[j])
     
     os.chdir( startDir )
-    vtus = ['vtus', 'vtus2']
+    #vtus = ['vtus', 'vtus2']
     #weights =  [0.5, 0.5]
 
     weights = []
-    for i in range(0,100):
-        weights.append(0.01)
+    for i in range(0, COUNTER):
+        weights.append(1.0/COUNTER)
 
-    MiscOps.IsoContourOperator('', 'init.vtu', result_vtus, weights)
+    MiscOps.IsoContourOperator(DIR, 'init.vtu', result_vtus, weights)
     
-    MiscOps.ImageSum(NAME+'*' + '.vti', True, 'imgSum.vti');
+    MiscOps.ImageSum(DIR + NAME+'*' + '.vti', True, 'imgSum.vti');
     
-        
+    shutil.move('./imgSum.vti', DIR + 'imgSum.vti')    
+    shutil.move('./isocontour_initial.vtp', DIR + 'isoncontour_initial.vtp')
+    shutil.move('./isocontour_mean.vtp', DIR + 'isocontour_mean.vtp')
+    shutil.move('./isocontour_inner.vtp', DIR + 'isocontour_inner.vtp')
+    shutil.move('./isocontour_outer.vtp', DIR + 'isocontour_outer.vtp')
 		
-   
+    #delet intermediate results folders
+    for i in range(1, COUNTER+1) :
+	    shutil.rmtree('./' + NAME + str(i))  
+ 
     #num_of_workers =   multiprocessing.cpu_count() -1
     #pool = multiprocessing.Pool(5)
     #pool.map(createAndRunHnnSimulation, args_list)
